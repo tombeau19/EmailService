deae2d3ecb941fe65f0b339f327e62932d29b7fd
Curtis Pittman <c.pittman@nbsupply.com>
2019-03-29 17:06:36 -0400

    Added Order class for input and can successfully connect to POST method

--- Controllers/TransactController.cs
+++ Controllers/TransactController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using BrontoTransactionalEndpoint.Models;
+using System.Collections.Generic;
 
 namespace BrontoTransactionalEndpoint.Controllers
 {
@@ -12,12 +9,62 @@ namespace BrontoTransactionalEndpoint.Controllers
     public class TransactController : ControllerBase
     {
 
-        // POST api/values
-        [HttpPost]
-        public string OrderConfirmation(string orderString)
+        // POST api/OrderConfirmation
+        [HttpPost("OrderConfirmation")]
+        public Order OrderConfirmation(Order input)
         {
-            return Bronto.OrderConfirmation(orderString);
+            //return Bronto.OrderConfirmation(input);
+            return input;
         }
 
+        [HttpGet("OrderConfirmationGet/{orderString}")]
+        public string OrderConfirmationGet(string orderString)
+        {
+            //return Bronto.OrderConfirmation(orderString);
+            return orderString;
+        }
+    }
+
+    public class Order
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string ShipAttn { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipAddress2 { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipState { get; set; }
+        public string ShipZip { get; set; }
+        public string ShipCountry { get; set; }
+        public string BillName { get; set; }
+        public string BillAddress { get; set; }
+        public string BillAddress2 { get; set; }
+        public string BillCity { get; set; }
+        public string BillState { get; set; }
+        public string BillZip { get; set; }
+        public string BillCountry { get; set; }
+        public string OrderNumber { get; set; }
+        public string OrderDate { get; set; }
+        public string Total { get; set; }
+        public string SubTotal { get; set; }
+        public string Tax { get; set; }
+        public string Shipping { get; set; }
+        public string OnlineSummaryLink { get; set; }
+        public List<LineItem> LineItems { get; set; }
+    }
+    public class LineItem
+    {
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string ShippingMessage { get; set; }
+        public string Note { get; set; }
+        public string ImageUrl { get; set; }
+        public string Discount { get; set; }
+        public bool BackOrdered { get; set; }
+        public bool ListSection { get; set; }
     }
 }
